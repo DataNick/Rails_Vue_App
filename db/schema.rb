e50c6e4fc760d333e8c2ed0a0b9cc93ebdae5d5b
DataNick <DataNick@users.noreply.github.com>
2018-06-12 23:57:53 -0400

    Modify columns, create card resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_13_020020) do
+ActiveRecord::Schema.define(version: 2018_06_13_034542) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "list_id"
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_cards_on_list_id"
   end
 
   create_table "lists", force: :cascade do |t|
