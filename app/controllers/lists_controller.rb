66bc4b3486aa0f198f004a38b292db34a0605ff2
DataNick <DataNick@users.noreply.github.com>
2018-06-13 21:47:06 -0400

    Add scope to sort list resources

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,7 +4,8 @@ class ListsController < ApplicationController
   # GET /lists
   # GET /lists.json
   def index
-    @lists = List.all
+    # @lists = List.order(position: :asc)
+    @lists = List.sorted
   end
 
   # GET /lists/1
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
   has_many :cards, -> { order(position: :asc) }, dependent: :destroy
+  scope :sorted, -> { order(position: :asc) }
+  validates :name, presence: true
 end
