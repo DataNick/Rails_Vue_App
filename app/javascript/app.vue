3d575e98f4dc317c8e41662d8b090d72929eee9f
DataNick <DataNick@users.noreply.github.com>
2018-06-16 22:29:14 -0400

    Add functionality to send AJAX post request and save to database

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,18 +1,52 @@
 <template>
   <div id="app" class="row">
-    <div v-for="list in original_lists" class="col-3">
+    <div v-for="(list, index) in original_lists" class="col-3">
       <h6>{{ list.name }}</h6>
       <hr />
-      <div v-for="(card, index) in list.cards" class="card card-body">
+      <div v-for="(card, index) in list.cards" class="card card-body mb-3">
         {{ card.name }}
+      </div>
+      <div class="card card-body">
+        <textarea class="form-control" v-model="messages[list.id]"></textarea>
+        <button class="btn btn-primary" @click="newCard(list.id)">New Card</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable';
 export default {
+
   props: ["original_lists"],
+  data: function(){
+    return {
+      messages: {},
+      lists: this.original_lists,
+    }
+  },
+  methods: {
+    newCard: function(list_id){
+      var data = new FormData;
+      data.append("card[list_id]", list_id);
+      data.append("card[name]", this.messages[list_id])
+      var xmessages = this.messages
+      Rails.ajax({
+        url: "/cards",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: (data) => {
+          const index = this.lists.findIndex(item => item.id == list_id)
+          this.lists[index].cards.push(data)
+          this.messages[list_id] = undefined
+        }
+      })
+    }
+  },
+  components: {
+    draggable
+  }
 
 }
 </script>
